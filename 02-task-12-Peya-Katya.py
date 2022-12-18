54ae49cfd2f32804b81634f2449b3271240f5085
legostaev <alexey.legostaev@gmail.com>
2022-12-18 11:57:27 +0300

    task 12 solution has been added

---
+++ 02-task-12-Peya-Katya.py
@@ -0,0 +1,24 @@
+# Задача 12 
+# Петя и Катя – брат и сестра. Петя – студент, а Катя – школьница.
+# Петя помогает Кате по математике. Он задумывает два натуральных числа X и Y (X,Y≤1000),
+# а Катя должна их отгадать. Для этого Петя делает две подсказки. Он называет сумму этих чисел S и их произведение P. 
+# Помогите Кате отгадать задуманные Петей числа.
+
+def LetsFindNums(sum, multiply: int):
+    for x in range(1001):
+        for y in range(1001):
+            if x + y == sum and x * y == multiply:
+                return x, y
+            else:
+                return ("not exist", "not exist")
+
+
+while True:
+    try:
+        S = int(input('enter the result of adding two numbers: '))
+        P = int(input('enter the result multiplication of two numbers: '))
+        nums = LetsFindNums(S, P)
+        print(f'X -> {nums[0]}, Y -> {nums[1]}')
+        break
+    except:
+        print('Oops! That was no valid numbers. Try again...')
